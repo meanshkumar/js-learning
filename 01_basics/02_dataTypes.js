3a72bd41ebf78740ac41dc3df1b801a870e7ff71
Ansh Kumar <im.ansh1410@gmail.com>
2024-11-26 23:28:29 +0000

    Data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict"
+//It states that the code written is a newer version of javascript
+
+console.log("Code Running")
+
+// alert("Hello") //we are using Node.js Not a browser (Used in browser)
+
+console.log(3+
+    3
+)
+//code readability issue . Readability should be given utmost priority 
+
+let name = "Ansh" //string
+let age = 21 // Number
+let isGraduated = false // boolean
+
+//Number => 2^52 range
+//bigInt => very large number used genrally in stocks or large website such as reddit
+//string =>" can be used in double" , 'or single quote' // prefer double quote
+//boolean => true/false
+//null => it is treated as an object // standalone value
+//undefined => when value is not assigned
+//symbol => unique
+//object 
+
+
+let temp =  null
+let un  
+
+console.log(typeof temp);
+console.log(typeof un);
+
+//null is an object
+//undefined gives undefined
+
+
+
